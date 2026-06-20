24fbb8c4c9c4445c17bf457201e20931e306c381
Harsh Maliye <harshmaliye2612@gmail.com>
2026-06-20 19:38:53 +0530

    First commit

---
+++ portfoliojs.js
@@ -0,0 +1,11 @@
+alert("Welcome to My Portfolio");
+
+console.log("Portfolio Loaded Successfully");
+
+document.querySelector("form").addEventListener("submit",function(event){
+
+event.preventDefault();
+
+alert("Thank You! Your message has been submitted.");
+
+});
